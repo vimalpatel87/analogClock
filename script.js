ba902d66695bc73f49a6410a06f78f41d37377ce
vimalpatel87 <vimalpatel@vimals-MacBook-Pro.local>
2017-12-29 01:56:25 -0600

    Analog clock with Javascript

---
+++ script.js
@@ -0,0 +1,16 @@
+const HOURHAND = document.querySelector("#hour");
+const MINUTEHAND = document.querySelector("#minute");
+const SECONDHAND = document.querySelector("#second");
+
+setInterval(function(){ var date = new Date();
+var sec = date.getSeconds();
+var min = date.getMinutes();
+var hr = date.getHours();
+
+let hrPosition = (hr * 360 / 12) + (min * (360 / 60)/12);
+let minPosition = (min * 360 / 60) + (sec * (360 / 60)/60);
+let secPosition = sec * 360 / 60;
+
+HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+SECONDHAND.style.transform = "rotate(" + secPosition + "deg)"; }, 100);
